0bae909a89d9b3bad456ee6049669cfcfa4dd835
Alim Mardaliev <mardaliev712@gmail.com>
2024-01-30 17:15:42 +0400

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+from random import randint
+
+robot_num = randint(1, 100)
+print('Угадайте число от 1 до 100')
+
+while True:
+    player_num = int(input('Ведите ваше число: '))
+    if robot_num == player_num:
+        print('Отличная интуиция! Вы угадали число :)')
+        break
+    elif robot_num > player_num:
+        print('Ваше число меньше того, что загадано)')
+        print('Попробуйте снова')
+    elif robot_num < player_num:
+        print('Ваше число больше того, что загадано')
+        print('Попробуйте снова')
